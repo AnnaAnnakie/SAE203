d3c0125453373685d7419a5633c4da62e8088bfb
Anna <anna.marchetti77@gmail.com>
2026-05-06 18:32:30 +0200

    update: header

--- sae203/includes/header.php
+++ sae203/includes/header.php
@@ -1,5 +1,6 @@
 <?php
-    $ernest = $_SESSION['user']['username'] ?? "Connexion";
+
+$ernest = $_SESSION['user']['username'] ?? null;
 ?>
 
 <header>
@@ -16,22 +17,14 @@
         </button>
     </form>
     <nav>
-
-        <div id="connexion">
-            <a href="" id="log-in">Se connecter</a>
-            <a href="" id="sign-up">S'inscrire</a>
-        </div>
-        <div id="connected" hidden>
-        <p>test</p>
-        </div>
-        <!--
-        <a<?php if (isset($_SESSION['user'])): ?> href="" <?php else: ?> href="" <?php endif; ?> id="connexion">
-            <?= $ernest ?>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                 class="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-            </svg>
-        </a>
-        -->
+        <?php if(!isset($_SESSION['user'])){ ?>
+            <div id="connexion">
+                <a href="/sae203/login.php" id="log-in">Se connecter</a>
+            </div>
+        <?php }else { ?>
+            <div id="connected">
+                <a href="/sae203/logout.php" id="logged"> <?=$ernest?></a>
+            </div>
+        <?php } ?>
     </nav>
 </header>
