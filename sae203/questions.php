2d55a849c5718a2c2b4d97e0927e22f630214f99
Anna <anna.marchetti77@gmail.com>
2026-05-06 18:33:41 +0200

    feat: quizz system

--- sae203/questions.php
+++ sae203/questions.php
@@ -3,46 +3,55 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/php/utils.php";
 if (empty($_GET["quizzId"])) {
     header("Location: index.php");
     exit();
-}else{
+} else {
     $quizzId = $_GET['quizzId'];
 
-    $queryQuestions = "SELECT * FROM `sae203_question` ques join marchean.sae203_quizz quizz on ques.quizz = quizz.id where quizz.id = $quizzId ";
-    $questions = getInfoDataBase($queryQuestions);
+    $questionsTab = getInfoDataBase("SELECT * FROM sae203_question WHERE quizz = $quizzId");
 
-    $questionId = 1;
-    $queryReponses = "SELECT * FROM sae203_reponse where question = $questionId ";
-    $reponses = getInfoDataBase($queryReponses);
+    $fullQuizData = [];
+
+    foreach ($questionsTab as $question) {
+        $qId = $question['id'];
+
+        $reponsesTab = getInfoDataBase("SELECT * FROM sae203_reponse WHERE question = $qId");
+
+        $fullQuizData[] = [
+                "id" => $qId,
+                "intitule" => $question['question'],
+                "reponses" => $reponsesTab
+        ];
+    }
 }
 ?>
 
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title></title>
-        <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/load-css.php"; ?>
-        <link rel="stylesheet" href="css/quizz.css">
-    </head>
-    <body>
-    <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/header.php"; ?>
-    <main>
-        <fieldset>
-            <legend><?=$questions[0]["question"] ?></legend>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/load-css.php"; ?>
+    <link rel="stylesheet" href="css/quizz.css">
+</head>
+<body>
+<?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/header.php"; ?>
+<main>
+    <fieldset>
+        <legend id="question-title"></legend>
 
-            <div class="reponses">
-                <?php
-                foreach ($reponses as $reponse) {
-                    echo "<div>";
-                    echo "<label for='" .$reponse["id"]."'>".$reponse["content"]."</label>";
-                    echo "<input type='checkbox' id='". $reponse['id']."' class='checkbox'>";
-                    echo "<img src='assets/check-mark.svg' alt='check'>";
-                    echo "</div>";
-                }
-                ?>
-            </div>
-            <button>VALIDER</button>
-        </fieldset>
-    </main>
-    <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/footer.php"; ?>
-    </body>
+        <div id="reponses">
+        </div>
+        <div id="btns">
+            <button id="prev-btn">ANNULER</button>
+            <button id="submit-btn">VALIDER</button>
+            <button id="next-btn" hidden>SUIVANTE</button>
+        </div>
+    </fieldset>
+</main>
+<?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/footer.php"; ?>
+</body>
+<script>
+    const dataset = <?php echo json_encode($fullQuizData); ?>;
+    console.log(dataset);
+</script>
+<script src="js/questions.js"></script>
 <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/load-js.php"; ?>
-    </html><?php
+</html>
--- sae203/quizz.php
+++ sae203/quizz.php
@@ -1,21 +1,26 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/php/utils.php";
+
+$quizz = $_GET['quizzId'];
+$query = "SELECT * FROM sae203_quizz WHERE id = '$quizz'";
+$quizzInfos = getInfoDataBase($query)[0];
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Question</title>
-        <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/load-css.php"; ?>
-    </head>
-    <body>
-        <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/header.php"; ?>
+<head>
+    <title>Question</title>
+    <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/load-css.php"; ?>
+</head>
+<body>
+<?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/header.php"; ?>
 
-        <main>
+<main>
+    <h1>Quizz <?= $quizzInfos['name'] ?></h1>
+    <a href="questions.php?quizzId=<?= $quizz ?>">Commencer</a>
+</main>
 
-        </main>
-
-        <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/footer.php"; ?>
-    </body>
+<?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/footer.php"; ?>
+</body>
 <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/sae203/includes/load-js.php"; ?>
 </html>
