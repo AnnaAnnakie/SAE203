bbeeda4ed59714a45dbcfeb3eadd413b31bb3b0a
Anna <anna.marchetti77@gmail.com>
2026-05-19 10:39:20 +0200

    refactor: remove useless search bar

--- sae203/includes/header.php
+++ sae203/includes/header.php
@@ -1,30 +1,30 @@
 <?php
-
+// On récupère le pseudo si l'utilisateur est connecté
 $ernest = $_SESSION['user']['username'] ?? null;
 ?>
 
 <header>
-    <a href="/sae203/">
+    <a href="/sae203/" class="logo-container">
         <img src="assets/zest.svg" alt="logo" width="150px">
     </a>
-    <form action="" method="get" id="search-bar">
-        <input type="text" class="input-group" name="query" placeholder="Rechercher...">
-        <button type="submit">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-search"
-                 viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-            </svg>
-        </button>
-    </form>
-    <nav>
-        <?php if(!isset($_SESSION['user'])){ ?>
-            <div id="connexion">
-                <a href="/sae203/login.php" id="log-in">Se connecter</a>
-            </div>
-        <?php }else { ?>
-            <div id="connected">
-                <a href="/sae203/profile.php" id="logged"> <?=$ernest?></a>
-            </div>
-        <?php } ?>
+
+    <nav class="nav-menu">
+        <ul class="nav-links">
+            <li><a href="/sae203/catalogue.php" class="nav-item">Catalogue</a></li>
+            <li><a href="/sae203/contact.php" class="nav-item">Contact</a></li>
+        </ul>
+
+        <div class="nav-auth">
+            <?php if (!$ernest): ?>
+                <div id="connexion">
+                    <a href="/sae203/login.php" id="log-in" class="btn-auth">Se connecter</a>
+                </div>
+            <?php else: ?>
+                <div id="connected" class="user-profile-badge">
+                    <span class="online-indicator"></span>
+                    <a href="/sae203/profile.php" id="logged"><?= htmlspecialchars($ernest) ?></a>
+                </div>
+            <?php endif; ?>
+        </div>
     </nav>
 </header>
